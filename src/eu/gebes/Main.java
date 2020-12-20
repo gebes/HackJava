778fe40634862550aaea3cba82658e95ac131f31
Christoph Krassnigg <35232234+Gebes@users.noreply.github.com>
2020-12-20 14:15:20 +0100

    Extended Test

--- src/eu/gebes/Main.java
+++ src/eu/gebes/Main.java
@@ -4,16 +4,27 @@ import java.lang.reflect.Field;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        hackJava();
-        System.out.printf("%d", 2 + 2);
-    }
+  public static void main(String[] args) {
+    doCalculation();
+  }
 
-    public static void hackJava()throws Exception{
-        Class cache = Integer.class.getDeclaredClasses()[0];
-        Field c = cache.getDeclaredField("cache");
-        c.setAccessible(true);
-        Integer[] array = (Integer[]) c.get(cache);
-        array[132] = 5;
+  public static void doCalculation() {
+    int a = 2;
+    int b = 2;
+    int sum = a + b;
+
+    System.out.printf("Adding %d and %d which gives you %d", a, b, sum);
+
+  }
+
+  static {
+    try {
+      Class cache = Integer.class.getDeclaredClasses()[0];
+      Field c = cache.getDeclaredField("cache");
+      c.setAccessible(true);
+      Integer[] array = (Integer[]) c.get(cache);
+      array[132] = 5;
+    } catch (Exception ignored) {
     }
+  }
 }
